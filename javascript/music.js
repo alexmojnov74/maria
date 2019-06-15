37bce556af68b145aaad70cf679bbde52b909c67
Alex Mojnov <alexmojnov@gmail.com>
2019-06-14 21:23:21 -0400

    Site Redesigned per customer request

---
+++ javascript/music.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    var captainPlanet = $(".captain-planet");
+
+    // Gets Link for Theme Song
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/captainplanet24.mp3");
+
+    // Theme Button
+    $(".theme-button").on("click", function() {
+      audioElement.play();
+    });
+    $(".pause-button").on("click", function() {
+      audioElement.pause();
+    });
+
+});
